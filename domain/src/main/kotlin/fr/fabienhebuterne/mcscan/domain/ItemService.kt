1c3579b30f61066d1f68d932501c3674ef3c9c4e
Fabien <contact@fabien-hebuterne.fr>
2021-02-20 18:58:50 +0100

    chore : remove useless method

--- domain/src/main/kotlin/fr/fabienhebuterne/mcscan/domain/ItemService.kt
+++ domain/src/main/kotlin/fr/fabienhebuterne/mcscan/domain/ItemService.kt
@@ -12,21 +12,10 @@ class ItemService(
 
         countItemService.getCounter()
             .toList()
-            .sortedBy { (_, value) -> value }
+            .sortedByDescending { (_, value) -> value }
             .toMap()
             .forEach { entry ->
                 itemRepository.create(ItemCounter(entry.key.toItemStorage(), entry.value.toLong()))
-            }
-    }
-
-    fun showItems() {
-        // TODO : Do not show here and create an output service with HTML generation or Console colored output
-        countItemService.getCounter()
-            .toList()
-            .sortedBy { (_, value) -> value }
-            .toMap()
-            .forEach { entry ->
-                println(entry.key.toString() + " : " + entry.value)
             }
     }
 
